1684b3413d03083023403563b4ae34fbd093a1e0
andrewdpoh <andrewdpoh@gmail.com>
2026-02-04 14:57:31 +0800

    Update db\crud.py
    Make db (aka SessionLocal()) into a global variable for the crud.py file. Remove db as a parameter for each function in crud.py

--- db/crud.py
+++ db/crud.py
@@ -1,8 +1,8 @@
 from db.database import SessionLocal
 from db.models import MedicalEvent, MedicalStatus, User, MovementLog
+db = SessionLocal()
 
 def get_user_by_telegram_id(telegram_id: int):
-    db = SessionLocal()
     try:
         return db.query(User).filter(User.telegram_id == telegram_id).first()
     finally:
@@ -10,20 +10,20 @@ def get_user_by_telegram_id(telegram_id: int):
         
 # Movement
 
-def create_medical_event(db, user_id, event_type, **kwargs):
+def create_medical_event(user_id, event_type, **kwargs):
     event = MedicalEvent(user_id=user_id, event_type=event_type, **kwargs)
     db.add(event)
     db.commit()
     db.refresh(event)
     return event
 
-def get_medical_event(db, today):
+def get_medical_event(today):
     return db.query(MedicalEvent).filter(
         MedicalEvent.start_datetime <= today,
         MedicalEvent.end_datetime >= today
     ).all()
 
-def get_active_statuses(db, today):
+def get_active_statuses(today):
     return db.query(MedicalStatus).filter(
         MedicalStatus.start_date <= today,
         MedicalStatus.end_date >= today
