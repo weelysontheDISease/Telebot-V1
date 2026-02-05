e6c25807b8f9be4b3713db549b56151af6c8c279
andrewdpoh <andrewdpoh@gmail.com>
2026-02-05 11:07:53 +0800

    Revert change of adding MC as a comment of event_type in models.py

--- db/models.py
+++ db/models.py
@@ -54,7 +54,7 @@ class MedicalEvent(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)  # telegram_id
 
-    event_type = Column(String, nullable=False) # MA / RSI / RSO / MC
+    event_type = Column(String, nullable=False) # MA / RSI / RSO
     appointment_type = Column(String)
     location = Column(String)
 
