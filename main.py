b2f66d566a4cebd47178e2585a062375be5fd39d
Yanliangchan <118790760+Yanliangchan@users.noreply.github.com>
2026-02-07 23:26:18 +0800

    Refactor command handlers and update imports
    Removed unused command handlers and added necessary imports.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from bot.cet import cet_handler
+from config.settings import BOT_TOKEN
+from bot.commands import start_sft, start_movement
+from bot.callbacks import callback_router, text_input_router
+from services.db_service import DatabaseService
+
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -55,11 +59,7 @@ def main():
     # -----------------------------
     dispatcher.add_handler(CommandHandler("start_sft", start_sft))
     dispatcher.add_handler(CommandHandler("start_movement", start_movement))
-    dispatcher.add_handler(CommandHandler("start_cet", start_cet))
-    dispatcher.add_handler(CommandHandler("start_status", start_status))
-    dispatcher.add_handler(CommandHandler("start_paradestate", start_parade_state))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, cet_handler))
-
 
     # -----------------------------
     # Callback Handlers (Buttons)
